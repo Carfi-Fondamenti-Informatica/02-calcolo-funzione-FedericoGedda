30cf3c966524c6be76367469ace6ca24afc8b8b3
FedericoGedda <115471970+FedericoGedda@users.noreply.github.com>
2022-11-15 14:41:35 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,9 +7,9 @@ int main() {
    if ((x<0)&&(y>0)) {
        cout<<z=(a*x)-(b*y);;
    } else if ((x>=0)&&(y<=0)){
-       cout<<z=a*x*x-b*y;
+       cout<<z=(a*x*x)-(b*y);
    } else {
-       cout<<z=a*x*+b*y*y;
+       cout<<z=(a*x)+(b*y*y);
    }
    return 0;
 }
