5708173a57a818619541e43629ac874aeba3ff64
FedericoGedda <115471970+FedericoGedda@users.noreply.github.com>
2022-11-15 14:30:17 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,17 @@
 using namespace std;
 
 int main() {
+   float a,b,x,y,z;
+   cin>>a>>b>>x>>y;
+   if ((x<0)&&(y>0)) {
+       z=(a*x)-(b*y);
+       cout<<z;
+   } else if ((x>=0)&&(y<=0)){
+       z=(a*x*x)-(b*y);
+       cout<<z;
+   } else {
+       z=(a*x)*(b*y*y);
+       cout<<z;
+   }
    return 0;
 }
